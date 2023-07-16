8afbee7498212d927dddd6870f760df5e62efb09
HarrisonMorris <102632477+HarrisonMorris@users.noreply.github.com>
2023-07-16 14:19:46 +1200

    printing leader board and 1st place score

--- src/Breakout/Welcome.java
+++ src/Breakout/Welcome.java
@@ -9,7 +9,7 @@ public class Welcome extends JPanel {
         setLayout(new BorderLayout());
     }
 
-    public void draw(Graphics g, Font atariFont, int GAME_WIDTH, int GAME_HEIGHT, String welcomeMessage, String modeMessage, String instructions) {
+    public void draw(Graphics g, Font atariFont, int GAME_WIDTH, int GAME_HEIGHT, String welcomeMessage, String modeMessage, String instructions, String leaderBoard) {
         FontMetrics fm = g.getFontMetrics(atariFont);
 
         g.setFont(atariFont);
@@ -23,11 +23,23 @@ public class Welcome extends JPanel {
 
         g.drawString(welcomeMessage, messageX, messageY);
         g.drawString(modeMessage, messageX, messageY + 40);
+        //g.drawString(leaderBoard, messageX, messageY + 100);
 
         // Draw instructions with line breaks
         String[] instructionLines = instructions.split("\n");
         for (int i = 0; i < instructionLines.length; i++) {
             String line = instructionLines[i];
+            int lineX = (GAME_WIDTH - fm.stringWidth(line)) / 2; // Calculate lineX based on the line width
+            int lineY = messageY + 60 + (i + 1) * fm.getHeight();
+            g.drawString(line, lineX, lineY);
+        }
+
+
+        // Draw Leader board with breaks
+
+        String[] leaderBoardLines = leaderBoard.split("\n");
+        for (int i = 0; i < leaderBoardLines.length; i++) {
+            String line = leaderBoardLines[i];
             int lineX = (GAME_WIDTH - fm.stringWidth(line)) / 2; // Calculate lineX based on the line width
             int lineY = messageY + 60 + (i + 1) * fm.getHeight();
             g.drawString(line, lineX, lineY);
